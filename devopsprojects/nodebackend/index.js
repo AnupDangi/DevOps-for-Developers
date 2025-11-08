d6c36cf0588307085d6c8e07cd28a58b1f0b3673
AnupDangi <anupdangi1589@gmail.com>
2025-11-08 15:32:37 +0545

    adding docker basics

---
+++ devopsprojects/nodebackend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app=express();
+
+const port=process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Send the request");
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is listening to port ${port}`);
+})
